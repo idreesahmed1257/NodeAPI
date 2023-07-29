1aa897bcee0457337fbfe4062f6cb2e88f2fc5cd
idrees ahmed <hidrees.bese25mcs@student.nust.edu.pk>
2023-07-29 11:22:27 +0500

    First Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send("Hello NodeAPI")
+})
+
+app.listen(3000, () => {
+    console.log('NodeAPI App is running on port 3000');
+})
